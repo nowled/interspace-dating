436fb6a7e67a154b65640c5fb41a4dc1e1ce5098
nowled <nowled@excite.com>
2021-03-22 22:44:50 +0300

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
+    "gatsby-plugin-pointer-events",
     {
       resolve: `gatsby-source-filesystem`,
       options: {
--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,63 @@
-import * as React from "react"
-import PropTypes from "prop-types"
+import React, { useEffect } from "react"
 import { Link } from "gatsby"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
+//Styled components
+
+import { HeaderNav, Logo, Menu } from "../styles/headerStyles"
+import { Container, Flex } from "../styles/globalStyles"
+
+import {
+  useGlobalStateContext,
+  useGlobalDispatchContext,
+} from "../context/globalContext"
+
+const Header = ({ onCursor }) => {
+  const dispatch = useGlobalDispatchContext()
+  const { currentTheme } = useGlobalStateContext()
+
+  const toggleTheme = () => {
+    if (currentTheme === "dark") {
+      dispatch({ type: "TOGGLE_THEME", theme: "light" })
+    } else {
+      dispatch({ type: "TOGGLE_THEME", theme: "dark" })
+    }
+  }
+
+  useEffect(() => {
+    window.localStorage.setItem("theme", currentTheme)
+  }, [currentTheme])
+
+  return (
+    <HeaderNav
+      animate={{ y: 0, opacity: 1 }}
+      initial={{ y: -72, opacity: 0 }}
+      transition={{ duration: 1, ease: [0.6, 0.05, -0.01, 0.9] }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+      <Container>
+        <Flex spaceBetween noHeight>
+          <Logo
+            onMouseEnter={() => onCursor("hovered")}
+            onMouseLeave={onCursor}
+          >
+            <Link to="/">INTERSPACE</Link>
+            <span
+              onClick={toggleTheme}
+              onMouseEnter={() => onCursor("pointer")}
+              onMouseLeave={onCursor}
+            ></span>
+            <Link to="/">DATING</Link>
+          </Logo>
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
+          <Menu>
+            <button>
+              <span></span>
+              <span></span>
+            </button>
+          </Menu>
+        </Flex>
+      </Container>
+    </HeaderNav>
+  )
 }
 
 export default Header
